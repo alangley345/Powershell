67af1d751261a6828be80ba49914a99a8364bd5b
alangley345 <39953972+alangley345@users.noreply.github.com>
2019-04-05 16:08:31 -0400

    Add files via upload

---
+++ get-dhcp.ps1
@@ -0,0 +1,12 @@
+#creates a call agaisnt the get-dhcpserverv4lease cmdlet and prompt users for input
+
+function get-dhcp
+{
+    $name = Read-Host -Prompt 'Input DHCP server name'
+    $scope = Read-host -Prompt 'Input desired scope'
+    $string = Read-Host -Prompt 'Input workstation name' 
+    get-dhcpserverv4lease -computername $name -scopeid $scope |out-string -stream | select-string $string
+    Read-Host -Prompt "Press Enter to exit"
+}
+
+get-dhcp
